cd6b71624055fb924d0206799875dbcc965b68fd
HrlCoder <634464975@qq.com>
2021-02-25 21:07:58 +0800

    IO

---
+++ ObjectStream/Person.java
@@ -0,0 +1,42 @@
+package ObjectStream;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private String name;
+    private int age;
+//    private static int age;
+//    private transient int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
